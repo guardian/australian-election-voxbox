51df5e1a513b35094db62d9d568dd1c1969fbe20
nickjevershed <nick.evershed@gmail.com>
2018-10-19 16:39:00 +1100

    uses a spreadsheet key

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,32 @@
 import xr from 'xr';
 import { Voxbox } from './modules/voxbox'
 
-var key = "10k7rSn5Y4x0V8RNyQ7oGDfhLvDqhUQ2frtZkDMoB1Xk"
+
+
+function getParameter(paramName) {
+	var searchString = window.location.search.substring(1),
+	i, val, params = searchString.split("&");
+
+	for (i=0;i<params.length;i++) {
+	val = params[i].split("=");
+	if (val[0] == paramName) {
+	return val[1];
+	}
+	}
+	return null;
+}
+
+var key = getParameter('key');
+
+if(!key) {
+	key = "10k7rSn5Y4x0V8RNyQ7oGDfhLvDqhUQ2frtZkDMoB1Xk";
+}
+
+console.log(key)
 
 xr.get('https://interactive.guim.co.uk/docsdata/' + key + '.json').then((resp) => {
 
 	let googledoc = resp.data.sheets;
-
 
 	new Voxbox(googledoc.data,googledoc.settings)
 	
